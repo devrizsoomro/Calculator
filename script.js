7774034b9a55e59469e2b27f45668e20f50647f7
devrizsoomro <rizwansoomro123456@gmail.com>
2024-10-12 17:40:24 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+ display.value = ""
+}
+function calculate() 
+{
+
+display.value = eval(display.value);
+}
+function  Del(){
+    display.value = "Del"
+}
+
+
+
